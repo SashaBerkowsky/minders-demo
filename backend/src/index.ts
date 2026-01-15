ae3d11dcebde49fbf013b71ab063529d90aa0783
SashaBerkowsky <snberkowsky@gmail.com>
2026-01-14 22:51:57 -0300

    remove comments & extra logs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(cors({
-    origin: '*', // For a public-facing SDK, '*' or a dynamic whitelist is common
+    origin: '*',
     methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-project-id'] // Add your custom headers here!
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-project-id']
 }));
 
 app.use(express.json())
