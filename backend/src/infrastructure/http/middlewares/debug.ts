a76176e98cb4d32a008eeb2728e19dbd7da2c95e
SashaBerkowsky <snberkowsky@gmail.com>
2026-01-22 12:41:52 -0300

    debug extra fields

--- backend/src/infrastructure/http/middlewares/debug.ts
+++ backend/src/infrastructure/http/middlewares/debug.ts
@@ -2,13 +2,14 @@ import { Request, Response, NextFunction } from 'express'
 import { config } from '@infrastructure/config'
 
 export const debugLogger = (req: Request, res: Response, next: NextFunction) => {
-    if (config.environment === 'debug') {
-        const start = Date.now();
-        res.on('finish', () => {
-            const duration = Date.now() - start;
-            console.debug(`[DEBUG] ${req.method} ${req.url} - ${res.statusCode} (${duration}ms)`);
-            console.debug(`[DEBUG] Body:`, JSON.stringify(req.body, null, 2));
-        });
-    }
-    next();
+  if (config.environment === 'debug') {
+    const start = Date.now();
+    res.on('finish', () => {
+      const duration = Date.now() - start;
+      console.debug(`[DEBUG] ${req.method} ${req.url} - ${res.statusCode} (${duration}ms)`);
+      console.debug(`[DEBUG] ProjectID: ${req.projectId} - Authorization: ${req.headers.authorization}`);
+      console.debug(`[DEBUG] Body:`, JSON.stringify(req.body, null, 2));
+    });
+  }
+  next();
 }
